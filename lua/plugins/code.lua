6c71d62bdda0e4195c8aa2d6d524630dcaa6e998
Evan Rittenhouse <emanguy1@gmail.com>
2025-06-15 23:21:55 -0400

    Add plugin for definition previews

---
+++ lua/plugins/code.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "rmagatti/goto-preview",
+    dependencies = { "rmagatti/logger.nvim" },
+    event = "BufEnter",
+    config = {
+      default_mappings = true,
+    },
+  },
+}
